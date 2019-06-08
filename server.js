f57953294f29d150d6b6350ad7bf8608baa4d852
Morgana Spake <morgana.spake@gmail.com>
2019-06-07 17:25:23 -0700

    lifegit add . (we can switch back and forth between players)

--- server.js
+++ server.js
@@ -4,12 +4,22 @@ const events = require('./events.js');
 
 const io = require('socket.io')(3000); //curried function
 
+const players = [];
+
 io.on('connect', (socket) => {
-  console.log('Connected!!!');
+  console.log('Connected!!!', socket.id);
+  players.push(socket.id);
 
   socket.on(events.move, payload => {
     console.log(payload);
-    io.emit(events.moved, 'It was MOVED.');
+
+    let playerWhoMoved = players.indexOf(socket.id);
+    if(playerWhoMoved === 0) {
+      io.to(`${players[1]}`).emit(events.moved, 'It was MOVED.');
+    }
+    if(playerWhoMoved === 1) {
+      io.to(`${players[0]}`).emit(events.moved, 'It was MOVED.');
+    }
   });
 });
 
