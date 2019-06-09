20e5d0cf0ca55ffcd694fd4ae3953b6049ffd049
Joé Jemmely <joe.jemmely@gmail.com>
2019-06-09 16:38:52 -0700

    Integrate game logic demo

--- nim-cli/index.js
+++ nim-cli/index.js
@@ -17,33 +17,96 @@ function init() {
 }
 
 /**
+ * Signup
+ */
+program
+  .command('signup')
+  .action(async () => {
+    user = await prompts([
+      {
+        type: 'text',
+        name: 'username',
+        message: 'Choose a username',
+      },
+      {
+        type: 'password',
+        name: 'password',
+        message: 'Choose a password',
+      },
+    ]);
+
+    console.log(`The account was created`);
+  })
+  .description('Create a new account');
+
+/**
  * Login
  */
-program.command('login').action(async () => {
-  user = await prompts([
-    {
-      type: 'text',
-      name: 'username',
-      message: 'Enter your username',
-    },
-    {
-      type: 'password',
-      name: 'password',
-      message: 'Enter your password',
-    },
-  ]);
+program
+  .command('login')
+  .action(async () => {
+    user = await prompts([
+      {
+        type: 'text',
+        name: 'username',
+        message: 'Enter your username',
+      },
+      {
+        type: 'password',
+        name: 'password',
+        message: 'Enter your password',
+      },
+    ]);
 
-  console.log(`Hi ${user.username}!`);
-});
+    console.log(`Hi ${user.username}!`);
+  })
+  .description('Login into your player account');
 
 /**
  * Logout
  */
-program.command('logout').action(async () => {
-  user
-    ? console.log('Goodbye')
-    : console.log('you need to be logged in to logout');
-});
+program
+  .command('logout')
+  .action(async () => {
+    user
+      ? console.log('Goodbye')
+      : console.log('you need to be logged in to logout');
+  })
+  .description('Logout from the current account');
+
+/**
+ * Play
+ */
+program
+  .command('play')
+  .action(async () => {
+    require('../game.js');
+  })
+  .description('Join or create a new game');
+
+/**
+ * Leaderboard
+ */
+program
+  .command('leaderboard')
+  .action(async () => {
+    const results = [
+      {
+        player: 'Player A',
+        score: 100,
+      },
+      {
+        player: 'Player B',
+        score: 90,
+      },
+      {
+        player: 'Player C',
+        score: 80,
+      },
+    ];
+    console.table(results);
+  })
+  .description('Show who\'s the best');
 
 async function run() {
   init();
